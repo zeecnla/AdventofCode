db1de4f58fe856c5d05c3f42c14b4febea0fe464
Cesar <csrmelchor@gmail.com>
2023-01-03 00:21:47 -0800

    Finished day 9.

--- 2022/day-9/day-9-2.rb
+++ 2022/day-9/day-9-2.rb
@@ -18,89 +18,12 @@ class Movement
    end
 end
 
-class Head
-
-   attr_accessor :row, :col
-
-   def initialize(row, col)
-     @row = row
-     @col = col
-   end
-end
-
-class Tail
-    attr_accessor :row, :col
-
-   def initialize(row, col)
-     @row = row
-     @col = col
-   end
-end
-
-File.readlines('day-9-sample.txt').each do |line|
+File.readlines('day-9-input-2.txt').each do |line|
    cleaned_line = line.chomp
    items = cleaned_line.split(" ")
 
    movements.push(Movement.new(items[1],items[0]))
 
-end
-
-
-
-def isAdjacent(head, item)
-
-   item_row = item[0]
-   item_col = item[1]
-
-   row = head[0]
-   col =head[1]
-
-   return true if row == item_row && col ==item_col
-   return true if (row-item_row).abs() <= 1 && (col-item_col).abs() <=1
-
-   return false
-end
-
-def checkAndMoveSnake(snake, head_locations,tail_locations, direction)
-    #make sure each item is adjacent from right to left
-
-      head = snake.last
-
-      snake.reverse.each_with_index  do |item,index|
-         next if item == head
-
-         if !isAdjacent(head,item)
-            if index == snake.length-1
-               tail_locations.push(item)
-            end
-
-
-            if !head_locations.empty?
-
-               snake[snake.length-1-index]= head_locations.pop()
-
-            else
-               row = item[0]
-               col = item[1]
-
-               if direction =="R"
-                  col = col +1
-               elsif direction =="U"
-                  row = row-1
-               elsif direction =="D"
-                  row = row+1
-               elsif direction =="L"
-                  col = col-1
-               end
-
-               snake[snake.length-1-index]= [row,col]
-
-
-            end
-            item = snake[snake.length-1-index]
-         end
-         head =item
-      end
 end
 
 snake = Array.new(10,[0,0])
@@ -113,11 +36,10 @@ movements.each do |movement|
    direction = movement.direction
    step = movement.step
 
-   first_step = step
 
    while (step >= 1) do
 
-      temp = snake.pop()
+      temp = snake[0]
 
       row = temp[0]
       col = temp[1]
@@ -132,24 +54,47 @@ movements.each do |movement|
          col = col-1
       end
 
-      snake.push([row,col])
+      snake[0] = [row,col]
 
-      if first_step == step
-         head_locations.push([row,col])
+      # (4,1) (2,2)
+      # delta : 2, -1
+      for i in 1...snake.length
+         #check each direction to check
+         delta_x = (snake[i-1][0] - snake[i][0])
+         delta_y = (snake[i-1][1] - snake[i][1])
+
+         if delta_x.abs() > 1 || delta_y.abs() >1
+
+            temp = snake[i]
+            new_row = temp[0]
+            new_col = temp[1]
+
+
+            if delta_x < 0
+               new_row = new_row + -1
+            elsif delta_x > 0
+               new_row = new_row + 1
+            end
+
+            if delta_y < 0
+               new_col = new_col + -1
+            elsif delta_y > 0
+               new_col = new_col + 1
+            end
+
+            snake[i] = [new_row,new_col]
+         end
+
       end
 
-      checkAndMoveSnake(snake,head_locations,tail_locations,direction)
-
       step = step -1
+      tail_locations.push(snake.last())
    end
 end
 
-puts snake.to_s
-
-
-puts tail_locations.uniq.to_s
 
 puts tail_locations.uniq.length
+
 
 
 
